8a814d4a9d4676b47001c4fdc7931e5e2757afc6
iszmxw <mail@54zm.com>
2024-01-19 23:44:36 +0800

    添加描述，以及添加options页面

---
+++ src/options.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AppWidget></AppWidget>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script setup lang="ts">
+import AppWidget from "./App.vue";
+
+defineOptions({
+  // prepare(app: App) {
+  //   // Use any plugins here:
+  //   // app.use
+  // },
+});
+</script>
