46b6c9af64ffce05d64773926e0c85c0560359d1
Ethan Yep <mail@54zm.com>
2024-01-25 18:29:13 +0800

    优化 newtab 和 options 页面引入组件的方法

--- src/newtab.vue
+++ src/newtab.vue
@@ -9,15 +9,15 @@
 <script setup lang="ts">
 // 引入element-plus插件与样式
 import { App } from 'vue';
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
-import AppWidget from './components/NewTab.vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import AppWidget from './components/NewTab.vue';
 
 defineOptions({
   prepare(app: App) {
     // Use any plugins here:
-    app.use(ElementPlus)
-    app.mount('#app')
+    app.use(ElementPlus);
+    app.mount('#app');
   },
 });
 </script>
--- src/options.vue
+++ src/options.vue
@@ -7,12 +7,17 @@
 <style></style>
 
 <script setup lang="ts">
+// 引入element-plus插件与样式
+import { App } from 'vue';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import Options from "./components/Options.vue";
 
 defineOptions({
-  // prepare(app: App) {
-  //   // Use any plugins here:
-  //   // app.use
-  // },
+  prepare(app: App) {
+    // Use any plugins here:
+    app.use(ElementPlus);
+    app.mount('#app');
+  },
 });
 </script>
