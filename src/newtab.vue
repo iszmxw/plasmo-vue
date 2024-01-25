9d08008cb64d14b7bfbb0b7a82fddffb574a4035
iszmxw <mail@54zm.com>
2024-01-25 21:55:28 +0800

    新窗口页面布局初始化

--- src/newtab.vue
+++ src/newtab.vue
@@ -4,13 +4,23 @@
   </div>
 </template>
 
-<style></style>
+<style>
+*,body{
+  margin: 0 !important;;
+  padding: 0 !important;;
+}
+html{
+  margin: 0 !important;;
+  padding: 0 !important;;
+}
+</style>
 
 <script setup lang="ts">
 // 引入element-plus插件与样式
 import { App } from 'vue';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
+import './style.css';
 import AppWidget from './components/NewTab.vue';
 
 defineOptions({
